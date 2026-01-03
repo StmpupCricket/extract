f05213bfacaffcbe05fe2732481b07de0cc1cef3
StmpupCricket <stmpuplinks@gmail.com>
2026-01-04 00:30:37 +0530

    Update extract.js

--- extract.js
+++ extract.js
@@ -2,7 +2,7 @@ import { chromium } from "playwright";
 import fs from "fs";
 
 const TARGET_URL = process.env.TARGET_URL || 
-  "https://www.fancode.com/football/tour/laliga-2025-26-18801700/matches/elche-cf-vs-villarreal-cf-131009/live-match-info";
+  "https://kompoz2.com/tv/610666/stunning-curvy-big-tit-pawg-cheating-wife-nika-venom.htmlp";
 
 const FOUND = new Set();
 const DEBUG_REQUESTS = [];
